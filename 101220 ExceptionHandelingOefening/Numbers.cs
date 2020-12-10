846f12f80257b382a6c208ffd4e400aa645f8b56
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2020-12-10 20:22:46 +0100

    .

--- 101220 ExceptionHandelingOefening/Numbers.cs
+++ 101220 ExceptionHandelingOefening/Numbers.cs
@@ -8,20 +8,46 @@ namespace _101220_ExceptionHandelingOefening
 {
     public class Numbers
     {
-        public static int Deel(int deeltal, int deler)
+        private static int vorigeInput { get; set; }
+
+        static Numbers()
         {
-            if (deler == 0)
-                throw new System.DivideByZeroException("Kan niet door 0 gedeeld worden");
-            int result = deler / deeltal;
-            return result;
+            vorigeInput = -1;
         }
-        public static double Wortel(double getal)
+        //public static int Deel(int deeltal, int deler)
+        //{
+        //    if (deler == 0)
+        //        throw new System.DivideByZeroException("Kan niet door 0 gedeeld worden");
+        //    int result = deler / deeltal;
+        //    return result;
+        //}
+        //public static double Wortel(double getal)
+        //{
+        //    if (getal < 0)
+
+        //        throw new System.ArgumentOutOfRangeException("Vierkantswortel van negatief getal kan niet");
+
+        //    return Math.Sqrt(getal);
+        //}
+
+        public static int ReadNumber( int start, int end)
         {
-            if (getal < 0)
+            Console.WriteLine($"Geef 10 getallen tussen de grenzen: {start}-{end}  en oplopend"); 
+            int input = 0;
 
-                throw new System.ArgumentOutOfRangeException("Vierkantswortel van negatief getal kan niet");
+            if (!Int32.TryParse(Console.ReadLine(), out input))
 
-            return Math.Sqrt(getal);
+                throw new System.ArgumentException($"Input is not valid INT");
+
+            if ((input < start || input > end))
+                throw new System.ArgumentException($"Ligt niet binnen grenzen {start}-{end}");
+
+            if (input <= vorigeInput)
+            {
+                throw new System.ArgumentException($"Input {input} moet groter zijn dan vorige input {vorigeInput}");
+            }
+            vorigeInput = input;
+            return input;   
         }
     }
 }
--- 101220 ExceptionHandelingOefening/Program.cs
+++ 101220 ExceptionHandelingOefening/Program.cs
@@ -8,33 +8,51 @@ namespace _101220_ExceptionHandelingOefening
 {
     class Program
     { 
-        static void Main()
+        static void Main(string[] args)
         {
+            int[] input = new int[10];
+
             try
             {
-                Console.WriteLine("\nGeef getal om Vierkantswortel te berekenen: ");
-                int getalC = Convert.ToInt32(Console.ReadLine());
-                Numbers.Wortel(getalC);
+                //Console.WriteLine("\nGeef getal om Vierkantswortel te berekenen: ");
+                //int getalC = Convert.ToInt32(Console.ReadLine());
+                //Numbers.Wortel(getalC);
 
-                Console.WriteLine("Geef deeltal: ");
-                int getalA = Convert.ToInt32(Console.ReadLine());
+                //Console.WriteLine("Geef deeltal: ");
+                //int getalA = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Geef deler: ");
-                int getalB = Convert.ToInt32(Console.ReadLine());
-                Numbers.Deel(getalA, getalB);
-                Console.WriteLine(Numbers.Deel(getalA, getalB));
+                //Console.WriteLine("Geef deler: ");
+                //int getalB = Convert.ToInt32(Console.ReadLine());
+                //Numbers.Deel(getalA, getalB);
+                //Console.WriteLine(Numbers.Deel(getalA, getalB));
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    input[i] = Numbers.ReadNumber(0, 100);
+                }
             }
 
-            catch (ArgumentOutOfRangeException ExCannotBeZero)
+            //catch (ArgumentOutOfRangeException ExCannotBeZero)
+            //{
+            //    Console.Error.WriteLine("Error: " + ExCannotBeZero.Message);
+            //    throw;
+            //}
+            //catch (DivideByZeroException ExNotDividedByZero)
+            //{
+            //    Console.Error.WriteLine("Error:" + ExNotDividedByZero.Message);
+            //    throw;
+            //}
+            catch (ArgumentException)
             {
-                Console.Error.WriteLine("Error: " + ExCannotBeZero.Message);
                 throw;
             }
-            catch (DivideByZeroException ExNotDividedByZero)
+            finally
             {
-                Console.Error.WriteLine("Error:" + ExNotDividedByZero.Message);
-                throw;
-            }    
+                foreach (var item in input)
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
